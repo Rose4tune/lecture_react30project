fa1e4b27aaadfdf87ca70bc1b4827e2dd6c84389
rose <rosefor2ne@gmail.com>
2023-09-13 00:15:35 +0900

    font 변경 구현

--- keyboard/src/js/keyboard.js
+++ keyboard/src/js/keyboard.js
@@ -1,5 +1,6 @@
 export class Keyboard {
   #switchEl;
+  #fontSelectEl;
   constructor(){
     this.#aasignElement();
     this.#addEvent();
@@ -7,10 +8,14 @@ export class Keyboard {
 
   #aasignElement(){ // 객체 가져오는 함수
     this.#switchEl = document.getElementById('switch');
+    this.#fontSelectEl = document.getElementById('font');
   }
   #addEvent(){ // 이벤트 실행 함수
     this.#switchEl.addEventListener("change", (event) => {
       document.documentElement.setAttribute("theme",event.target.checked ? "dark-mode" : "")
+    })
+    this.#fontSelectEl.addEventListener("change", (event) => {
+      document.body.style.fontFamily = event.target.value;
     })
   }
 }
