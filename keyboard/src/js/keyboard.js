cb58664fc61f6523130ecc7874bf9a5ef4e4a77d
rose <rosefor2ne@gmail.com>
2023-09-13 22:46:33 +0900

    key down/up 이벤트, 한글 입력 제한 이벤트 구현

--- keyboard/src/js/keyboard.js
+++ keyboard/src/js/keyboard.js
@@ -2,6 +2,9 @@ export class Keyboard {
   #switchEl;
   #fontSelectEl;
   #containerEl;
+  #keyboardEl;
+  #inputGroupEl;
+  #inputEl;
   constructor(){
     this.#aasignElement();
     this.#addEvent();
@@ -11,13 +14,31 @@ export class Keyboard {
     this.#containerEl = document.getElementById('container');
     this.#switchEl = this.#containerEl.querySelector('#switch');
     this.#fontSelectEl = this.#containerEl.querySelector('#font');
+    this.#keyboardEl = this.#containerEl.querySelector('#keyboard');
+    this.#inputGroupEl = this.#containerEl.querySelector('#input-group');
+    this.#inputEl = this.#inputGroupEl.querySelector('#input');
   }
   #addEvent(){ // 이벤트 실행 함수
-    this.#switchEl.addEventListener("change", this.#onChangeTheme)
-    this.#fontSelectEl.addEventListener("change", this.#onChangeFont)
+    this.#switchEl.addEventListener("change", this.#onChangeTheme);
+    this.#fontSelectEl.addEventListener("change", this.#onChangeFont);
+    document.addEventListener("keydown", (event)=> {
+      this.#inputGroupEl.classList.toggle("error", /[ㄱ-ㅎ|ㅏ-ㅣ|가-힣]/.test(event.key))
+      this.#keyboardEl
+        .querySelector(`[data-code=${event.code}]`)
+        ?.classList.add('active');
+    });
+    document.addEventListener("keyup", (event)=> {
+      this.#keyboardEl
+        .querySelector(`[data-code=${event.code}]`)
+        ?.classList.remove('active');
+    });
+    this.#inputEl.addEventListener("input", (event) => {
+      this.#inputEl.value = this.#inputEl.value.replace(/[ㄱ-ㅎ|ㅏ-ㅣ|가-힣]/, "");
+      
+    })
   }
   #onChangeTheme(event){
-    document.documentElement.setAttribute("theme", event.target.checked ? "dark-mode" : "")
+    document.documentElement.setAttribute("theme", event.target.checked ? "dark-mode" : "");
   }
   #onChangeFont(event){
     document.body.style.fontFamily = event.target.value;
