3b142afaf89a3f0ab81c966f6b3b4925833feedb
Lee Ye Seo <rosefor2ne@gmail.com>
2023-09-13 17:36:01 +0900

    객체 가져오는 함수 리팩토링

--- keyboard/src/js/keyboard.js
+++ keyboard/src/js/keyboard.js
@@ -1,14 +1,16 @@
 export class Keyboard {
   #switchEl;
   #fontSelectEl;
+  #containerEl;
   constructor(){
     this.#aasignElement();
     this.#addEvent();
   }
 
   #aasignElement(){ // 객체 가져오는 함수
-    this.#switchEl = document.getElementById('switch');
-    this.#fontSelectEl = document.getElementById('font');
+    this.#containerEl = document.getElementById('container');
+    this.#switchEl = this.#containerEl.querySelector('switch');
+    this.#fontSelectEl = this.#containerEl.querySelector('font');
   }
   #addEvent(){ // 이벤트 실행 함수
     this.#switchEl.addEventListener("change", (event) => {
