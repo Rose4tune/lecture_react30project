3df9ebe0f9f1c96ef701d391f5bb7a27a123cf29
rose <rosefor2ne@gmail.com>
2023-09-13 23:23:13 +0900

    mouse down/up 이벤트 구현

--- keyboard/src/js/keyboard.js
+++ keyboard/src/js/keyboard.js
@@ -23,7 +23,32 @@ export class Keyboard {
     this.#fontSelectEl.addEventListener("change", this.#onChangeFont);
     document.addEventListener("keydown", this.#onKeyDown.bind(this));
     document.addEventListener("keyup", this.#onKeyUp.bind(this));
-    this.#inputEl.addEventListener("input", this.#onInput)
+    this.#inputEl.addEventListener("input", this.#onInput);
+    this.#keyboardEl.addEventListener("mousedown", this.#onMouseDown);
+    document.addEventListener("mouseup", this.#onMouseUp.bind(this));
+  }
+
+  #onMouseUp(event){
+    const keyEl = event.target.closest("div.key");
+    const isActive = !!keyEl?.classList.contains("active");//타입캐스팅
+    const val = keyEl?.dataset.val;
+
+    if(isActive && !!val && val !== "Space" && val !== "Backspace"){
+      this.#inputEl.value += val;
+    }
+    if(isActive && val === "Space") {
+      this.#inputEl.value += " ";
+    }
+    if(isActive && val === "Backspace") {
+      this.#inputEl.value = this.#inputEl.value.slice(0, -1);
+    }
+    this.#keyboardEl.querySelector(".active")
+      ?.classList.remove("active");
+  }
+
+  #onMouseDown(event){
+    event.target.closest("div.key")
+      ?.classList.add("active");
   }
 
   #onInput(event){
