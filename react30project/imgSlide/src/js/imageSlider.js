05bc832fdfb39f2d649baf8d62332ec88827b0ef
Lee Ye Seo <rosefor2ne@gmail.com>
2023-09-19 11:30:42 +0900

    next, prev 버튼 구현

---
+++ react30project/imgSlide/src/js/imageSlider.js
@@ -0,0 +1,69 @@
+export default class ImageSlider {
+  #currentPosition = 0;
+
+  #slideNumber = 0;
+
+  #slideWidth = 0;
+
+  sliderWrapEl;
+
+  sliderListEl;
+
+  nextBtnEl;
+
+  prevBtnEl;
+
+  constructor() {
+    this.assignElement();
+    this.initSliderNumber();
+    this.initSlideWidth();
+    this.initSliderListWidth();
+    this.addEvent();
+  }
+
+  assignElement() {
+    this.sliderWrapEl = document.getElementById('slider-wrap');
+    this.sliderListEl = this.sliderWrapEl.querySelector('#slider');
+    this.nextBtnEl = this.sliderWrapEl.querySelector('#next');
+    this.prevBtnEl = this.sliderWrapEl.querySelector('#previous');
+  }
+
+  // 슬라이드 넘버 초기화
+  initSliderNumber() {
+    this.#slideNumber = this.sliderListEl.querySelectorAll('li').length;
+  }
+
+  // 슬라이드 가로크기 초기화
+  initSlideWidth() {
+    this.#slideWidth = this.sliderListEl.clientWidth;
+  }
+
+  initSliderListWidth() {
+    this.sliderListEl.style.width = `${this.#slideNumber * this.#slideWidth}px`;
+  }
+
+  addEvent() {
+    this.nextBtnEl.addEventListener('click', this.moveToRight.bind(this));
+    this.prevBtnEl.addEventListener('click', this.moveToLeft.bind(this));
+  }
+
+  moveToRight() {
+    this.#currentPosition += 1;
+    if (this.#currentPosition === this.#slideNumber) {
+      this.#currentPosition = 0;
+    }
+    this.sliderListEl.style.left = `-${
+      this.#slideWidth * this.#currentPosition
+    }px`;
+  }
+
+  moveToLeft() {
+    this.#currentPosition -= 1;
+    if (this.#currentPosition === -1) {
+      this.#currentPosition = this.#slideNumber - 1;
+    }
+    this.sliderListEl.style.left = `-${
+      this.#slideWidth * this.#currentPosition
+    }px`;
+  }
+}
