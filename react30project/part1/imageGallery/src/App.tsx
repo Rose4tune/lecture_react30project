f2a00d4ffbf4a7272f67d6ecc8be650f4ccdd363
rose <rosefor2ne@gmail.com>
2023-10-11 22:30:57 +0900

    image box 컴포넌트 만들기

--- react30project/part1/imageGallery/src/App.tsx
+++ react30project/part1/imageGallery/src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import ImageBox from './component/ImageBox';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         </div>
         <div className='plus-box'>+</div>
       </div>
+      <ImageBox src="aa"/>
     </div>
   );
 }
