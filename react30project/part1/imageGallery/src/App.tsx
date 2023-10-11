3484b051c2b323e219928513763a8a0e038bfd6f
rose <rosefor2ne@gmail.com>
2023-10-11 22:37:17 +0900

    file input 추가 및 plus box 클릭 이벤트 구현

--- react30project/part1/imageGallery/src/App.tsx
+++ react30project/part1/imageGallery/src/App.tsx
@@ -1,19 +1,27 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import ImageBox from './component/ImageBox';
 
 function App() {
+  const inputRef = useRef<HTMLInputElement>(null);
+
   return (
     <div className='container'>
       <div className='initial-box'>
-        <div className='text-center'>
-          이미지가 없습니다.<br/>
-          이미지를 추가해주세요
+          <div className='text-center'>
+            이미지가 없습니다.<br/>
+            이미지를 추가해주세요
+          </div>
+        <input type="file" ref={inputRef}/>
+        <div className='plus-box'
+          onClick = {() => {
+            inputRef.current?.click()
+          }}
+        >
+          +
         </div>
-        <div className='plus-box'>+</div>
       </div>
-      <ImageBox src="aa"/>
     </div>
   );
 }
