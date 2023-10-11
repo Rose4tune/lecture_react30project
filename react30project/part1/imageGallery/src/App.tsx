2f049254343ef78c68f0cbc97dbe597ab8596e8b
rose <rosefor2ne@gmail.com>
2023-10-11 22:39:26 +0900

    input 파일 받아서 배열 만드는 이벤트 구현

--- react30project/part1/imageGallery/src/App.tsx
+++ react30project/part1/imageGallery/src/App.tsx
@@ -1,10 +1,11 @@
-import React, { useRef } from 'react';
+import React, { useRef, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import ImageBox from './component/ImageBox';
 
 function App() {
   const inputRef = useRef<HTMLInputElement>(null);
+  const [imageList, setImageList] = useState<string[]>([]);
 
   return (
     <div className='container'>
@@ -13,7 +14,14 @@ function App() {
             이미지가 없습니다.<br/>
             이미지를 추가해주세요
           </div>
-        <input type="file" ref={inputRef}/>
+        <input type="file" ref={inputRef}
+          onChange = {event => {
+            if(event.currentTarget.value) {
+              const v = event.currentTarget.value;
+              setImageList(prev => [...prev, v])
+            }
+          }}
+        />
         <div className='plus-box'
           onClick = {() => {
             inputRef.current?.click()
