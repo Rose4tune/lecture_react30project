e148dc6e6fa6052e0379708f6d02524ae2e4000a
rose <rosefor2ne@gmail.com>
2023-10-29 15:32:56 +0900

    브러쉬 선택 이벤트

--- react30project/part2/drawingBoard/dist/index.de5c0784.js
+++ react30project/part2/drawingBoard/dist/index.de5c0784.js
@@ -1,3 +1,29 @@
-console.log("\uADF8\uB9BC\uD310!!");
+class DrawingBoard {
+    MODE = "NONE";
+    constructor(){
+        this.assingElement();
+        this.initContext();
+        this.addEvent();
+    }
+    assingElement() {
+        this.containerEl = document.getElementById("container");
+        this.canvasEl = this.containerEl.querySelector("#canvas");
+        this.toolbarEl = this.containerEl.querySelector("#toolbar");
+        this.brushEl = this.toolbarEl.querySelector("#brush");
+    }
+    initContext() {
+        this.context = this.canvasEl.getContext("2d");
+    }
+    addEvent() {
+        this.brushEl.addEventListener("click", this.onClickBrush.bind(this));
+    }
+    onClickBrush(event) {
+        const IsActive = event.currentTarget.classList.contains("active");
+        this.MODE = IsActive ? "NONE" : "BRUSH";
+        this.canvasEl.style.cursor = IsActive ? "default" : "crosshair";
+        this.brushEl.classList.toggle("active");
+    }
+}
+new DrawingBoard();
 
 //# sourceMappingURL=index.de5c0784.js.map
