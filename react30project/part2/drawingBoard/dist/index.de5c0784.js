0732541b938af6730f15f117619bd71214e589e6
rose <rosefor2ne@gmail.com>
2023-10-29 16:03:41 +0900

    선 그리기 구현

--- react30project/part2/drawingBoard/dist/index.de5c0784.js
+++ react30project/part2/drawingBoard/dist/index.de5c0784.js
@@ -1,5 +1,6 @@
 class DrawingBoard {
     MODE = "NONE";
+    IsMouseDown = false;
     constructor(){
         this.assingElement();
         this.initContext();
@@ -16,9 +17,39 @@ class DrawingBoard {
     }
     addEvent() {
         this.brushEl.addEventListener("click", this.onClickBrush.bind(this));
+        this.canvasEl.addEventListener("mousedown", this.onMouseDown.bind(this));
+        this.canvasEl.addEventListener("mousemove", this.onMouseMove.bind(this));
+        this.canvasEl.addEventListener("mouseup", this.onMouseUp.bind(this));
     }
-    onClickBrush(event) {
-        const IsActive = event.currentTarget.classList.contains("active");
+    onMouseDown(event1) {
+        if (this.MODE === "NONE") return;
+        this.IsMouseDown = true;
+        const currentPosition = this.getMousePosition(event1);
+        this.context.beginPath();
+        this.context.moveTo(currentPosition.x, currentPosition.y);
+        this.context.lineCap = "round";
+        this.context.strokeStyle = "#000000";
+        this.context.lineWidth = 10;
+    }
+    onMouseMove(event1) {
+        if (!this.IsMouseDown) return;
+        const currentPosition = this.getMousePosition(event1);
+        this.context.lineTo(currentPosition.x, currentPosition.y);
+        this.context.stroke();
+    }
+    onMouseUp() {
+        if (this.MODE === "NONE") return;
+        this.IsMouseDown = false;
+    }
+    getMousePosition() {
+        const boundaries = this.canvasEl.getBoundingClientRect();
+        return {
+            x: event.clientX - boundaries.left,
+            y: event.clientY - boundaries.top
+        };
+    }
+    onClickBrush(event1) {
+        const IsActive = event1.currentTarget.classList.contains("active");
         this.MODE = IsActive ? "NONE" : "BRUSH";
         this.canvasEl.style.cursor = IsActive ? "default" : "crosshair";
         this.brushEl.classList.toggle("active");
--- react30project/part2/drawingBoard/src/js/index.js
+++ react30project/part2/drawingBoard/src/js/index.js
@@ -1,11 +1,13 @@
 class DrawingBoard {
   MODE = "NONE"; // NONE BRUSH ERASER
+  IsMouseDown = false;
 
   constructor() {
     this.assingElement();
     this.initContext();
     this.addEvent();
   }
+  
   assingElement() {
     this.containerEl = document.getElementById('container');
     this.canvasEl = this.containerEl.querySelector('#canvas');
@@ -17,7 +19,43 @@ class DrawingBoard {
   }
   addEvent() {
     this.brushEl.addEventListener("click", this.onClickBrush.bind(this));
+    this.canvasEl.addEventListener("mousedown", this.onMouseDown.bind(this));
+    this.canvasEl.addEventListener("mousemove", this.onMouseMove.bind(this));
+    this.canvasEl.addEventListener("mouseup", this.onMouseUp.bind(this));
   }
+
+  onMouseDown(event) {
+    if(this.MODE === "NONE") return;
+    this.IsMouseDown = true;
+
+    const currentPosition = this.getMousePosition(event);
+    this.context.beginPath();
+    this.context.moveTo(currentPosition.x, currentPosition.y);
+    this.context.lineCap = "round";
+    this.context.strokeStyle = "#000000";
+    this.context.lineWidth = 10;
+  }
+
+  onMouseMove(event) {
+    if(!this.IsMouseDown) return;
+    const currentPosition = this.getMousePosition(event);
+    this.context.lineTo(currentPosition.x, currentPosition.y);
+    this.context.stroke();
+  }
+
+  onMouseUp() {
+    if(this.MODE === "NONE") return;
+    this.IsMouseDown = false;
+  }
+
+  getMousePosition() {
+    const boundaries = this.canvasEl.getBoundingClientRect();
+    return {
+      x: event.clientX - boundaries.left,
+      y: event.clientY - boundaries.top
+    }
+  }
+
   onClickBrush(event) {
     const IsActive = event.currentTarget.classList.contains('active')
     this.MODE = IsActive ? "NONE" : "BRUSH";
