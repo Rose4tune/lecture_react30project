08c6ee114495732e24372ba334de85c2f49eb4d6
rose <rosefor2ne@gmail.com>
2023-10-30 21:04:46 +0900

    rollup.js 기본 실행 셋팅

---
+++ react30project/part2/todoList/dist/bundle.js
@@ -0,0 +1,26 @@
+
+(function(l, r) { if (!l || l.getElementById('livereloadscript')) return; r = l.createElement('script'); r.async = 1; r.src = '//' + (self.location.host || 'localhost').split(':')[0] + ':35729/livereload.js?snipver=1'; r.id = 'livereloadscript'; l.getElementsByTagName('head')[0].appendChild(r) })(self.document);
+'use strict';
+
+
+
+function ___$insertStylesToHeader(css) {
+  if (!css) {
+    return
+  }
+  if (typeof window === 'undefined') {
+    return
+  }
+
+  const style = document.createElement('style');
+
+  style.setAttribute('type', 'text/css');
+  style.innerHTML = css;
+  document.head.appendChild(style);
+  return css
+}
+
+___$insertStylesToHeader("h1 {\n  text-transform: uppercase;\n  color: aqua;\n}");
+
+console.log('todo list');
+//# sourceMappingURL=bundle.js.map
